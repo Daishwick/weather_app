3609f6068f208b59ae9d0565d26878030b4f42a3
Daishwick <87265580+Daishwick@users.noreply.github.com>
2022-04-20 15:28:13 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import Weather from "./Weather";
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The weather</h1>
-          <Weather />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="weather-container" id="weather">
+                <h1>The weather</h1>
+                <Weather/>
+            </div>
+            <footer className="source-code">
+                <a href="https://github.com/Daishwick/weather_app" target="_blank">
+                    Open-source code
+                </a>, by
+                Jitske Riswick
+            </footer>
+        </div>
+    );
 }
 
 
