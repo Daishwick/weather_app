988ed81493cd5068023921d111700290e7498d64
Daishwick <87265580+Daishwick@users.noreply.github.com>
2022-05-12 14:28:47 +0200

    CSS fixed

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,25 +15,21 @@ export default function WeatherInfo(props) {
                     {props.data.description}
                 </li>
             </ul>
-            <div className="row mt-3">
-                <div className="col-6">
-                    <div className="clearfix">
-                        <div className="float-left">
-                            <WeatherIcon code={props.data.icon} size={52} />
-                        </div>
+            <div className="row">
+                <div className="col-1">
+                    <WeatherIcon code={props.data.icon} size={52}/>
+                </div>
+                <div className="col-4">
+                    <WeatherTemperature celsius={props.data.temperature}/>
+                </div>
 
-                        <div className="float-left">
-                            <WeatherTemperature celsius={props.data.temperature} />
-                        </div>
-                    </div>
+                <div className="col humidityWind">
+                    <ul>
+                        <li>Humidity: {props.data.humidity}%</li>
+                        <li>Wind: {props.data.wind} km/h</li>
+                    </ul>
                 </div>
-                        <div className="col-6">
-                            <ul>
-                                <li> Humidity: {props.data.humidity}%</li>
-                                <li> Wind: {props.data.wind} km/h</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                );
-                }
+            </div>
+        </div>
+    );
+}
